8e2c19f91687f4407370f5a63de33a4035bb7b66
lionet <jialecode@qq.com>
2023-02-17 22:47:47 +0800

    chore: 调整eslint, commitlint

--- src/page.js
+++ src/page.js
@@ -1,10 +1,11 @@
+/* eslint-env browser */
 // 控制页面
 
-const isConsole = location.host === 'console.lionet.cloud';
+const isConsole = window.location.host === 'console.lionet.cloud';
 
-function initialize(){
-  if(!isConsole){
-    return; 
+function initialize() {
+  if (!isConsole) {
+    return;
   }
 }
 
